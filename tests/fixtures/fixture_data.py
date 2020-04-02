b0c8148614574c61da218e48a6808565c256fdf3
Vell <dagornet@gmail.com>
2020-04-02 13:53:49 +0500

    🎉 initial

--- tests/fixtures/fixture_data.py
+++ tests/fixtures/fixture_data.py
@@ -1,21 +1,12 @@
-import tempfile
-
 import pytest
+import tempfile
 
 
 @pytest.fixture
 def post(user):
     from posts.models import Post
     image = tempfile.NamedTemporaryFile(suffix=".jpg").name
-    return Post.objects.create(text='Тестовый пост 1',
-                               author=user, image=image)
-
-
-@pytest.fixture
-def post_2(user, group_1):
-    from posts.models import Post
-    return Post.objects.create(text='Тестовый пост 2',
-                               author=user, group=group_1)
+    return Post.objects.create(text='Тестовый пост 1', author=user, image=image)
 
 
 @pytest.fixture
@@ -27,8 +18,7 @@ def comment_1_post(post, user):
 @pytest.fixture
 def comment_2_post(post, another_user):
     from posts.models import Comment
-    return Comment.objects.create(author=another_user, post=post,
-                                  text='Коммент 2')
+    return Comment.objects.create(author=another_user, post=post, text='Коммент 2')
 
 
 @pytest.fixture
@@ -40,17 +30,4 @@ def another_post(another_user):
 @pytest.fixture
 def comment_1_another_post(another_post, user):
     from posts.models import Comment
-    return Comment.objects.create(author=user, post=another_post,
-                                  text='Коммент 12')
-
-
-@pytest.fixture
-def group_1():
-    from posts.models import Group
-    return Group.objects.create(title='Группа 1', slug='group_1')
-
-
-@pytest.fixture
-def group_2():
-    from posts.models import Group
-    return Group.objects.create(title='Группа 2', slug='group_2')
+    return Comment.objects.create(author=user, post=another_post, text='Коммент 12')
