286a9ad58308fb9f0f1faa4d33c92a023da7c2c3
TurboKach <incognito@turbokach.me>
2021-06-10 18:34:18 +0300

    changed project struct

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,15 +4,6 @@ from django.db import models
 User = get_user_model()
 
 
-class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(
@@ -24,10 +15,6 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True
     )  # поле для картинки
-    group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL,
-        related_name='posts', blank=True, null=True
-    )
 
     def __str__(self):
         return self.text
